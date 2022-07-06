552d59b172a4ee49c3b73d9b71e09e3822b7b7c6
Modassir Ali <alimodassir@gmail.com>
2022-07-06 11:24:20 +0530

    commented logics

--- youtube/youtubeApi.js
+++ youtube/youtubeApi.js
@@ -5,12 +5,16 @@ const Videos = require("../models/Videos");
 
 var { API_KEYS } = require("./youtubeUtils");
 
+//Rotating array, to change API KEYS
 const rotateArray = () => {
   API_KEYS.unshift(API_KEYS.pop());
 };
 
+//Fetching all videos from Youtube
 module.exports.getYTVideos = async () => {
+  //Randomly getting a query from an array.
   const query = QUERIES[Math.floor(Math.random() * QUERIES?.length)];
+
   const URL = `${BASE_URL}/search?key=${API_KEYS[0]}&type=video&part=snippet&q=${query}`;
   try {
     const res = await axios.get(URL);
@@ -29,6 +33,9 @@ module.exports.getYTVideos = async () => {
     });
     await Videos.insertMany(allVideos);
   } catch (error) {
+    /*If quota is exhausted, then 403 status code will return, so will 
+    change the API Keys by rotating the API_KEYS array 
+    */
     if (error?.response?.status == 403) {
       rotateArray();
     } else console.log(error);
